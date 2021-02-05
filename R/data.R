ccccb871015ae95fdf1c858c9f69ec96a39eefa6
plger <pl.germain@gmail.com>
2021-02-05 08:38:32 +0000

    added data documentation

---
+++ R/data.R
@@ -0,0 +1,26 @@
+#' @rdname example_bin_se
+#' @name example_bin_se
+#' @aliases example_bin_se
+#'
+#' @title Example bin-level `RangedSummarizedExperiment`
+#'
+#' @description
+#' An object produced by \code{\link{countFeatures}} containing small subset of 
+#' genes from mouse hippocampal slices undergoing Forskolin-induced long-term 
+#' potentiation (GSE84643).
+#'
+#' @return a `RangedSummarizedExperiment`
+#' @references \url{https://www.nature.com/articles/s41598-017-17407-w}
+NULL
+
+#' @rdname example_gene_annotation
+#' @name example_gene_annotation
+#' @aliases example_gene_annotation
+#'
+#' @title Example gene annotation
+#'
+#' @description
+#' An example gene annotation containing only a small subset of mouse genes.
+#'
+#' @return a `GRanges` object
+NULL
