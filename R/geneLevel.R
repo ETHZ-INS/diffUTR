d0118c2b77f87e9e844c2b85477b8a52e57981b2
plger <pl.germain@gmail.com>
2021-04-06 12:09:27 +0000

    major geneLevel speedup; fixed wrong default coef for DEXSeq

--- R/geneLevel.R
+++ R/geneLevel.R
@@ -81,8 +81,10 @@ geneLevelStats <- function(se, coef=NULL, excludeTypes=NULL, includeTypes=NULL,
   if(!is.null(excludeTypes)){
     rd$bin.p.value[which(rd$type %in% excludeTypes)] <- 1
   }
-  if(is.null(coef))
-    coef <- colnames(rd)[grep("bin.p.value",colnames(rd))-1]
+  if(is.null(coef)){
+    tmp <- setdiff(colnames(rd),"exonBaseMean")
+    coef <- colnames(rd)[grep("bin.p.value",tmp)-1]
+  }
 
   metadata(se)$geneLevel <- .geneLevelStats(DataFrame(
     bin.pval=rd$bin.p.value, coef=rd[[coef]], width=width(se),
@@ -93,33 +95,37 @@ geneLevelStats <- function(se, coef=NULL, excludeTypes=NULL, includeTypes=NULL,
 }
 
 #' @importFrom methods is as
+#' @importFrom BiocParallel bpnworkers
+#' @importFrom IRanges LogicalList NumericList IntegerList
 .geneLevelStats <- function(d, gene.qval=NULL){
   stopifnot(c("bin.pval","coef","gene","width","meanLogDensity") %in%
               colnames(d))
   d$bin.pval[is.na(d$bin.pval)] <- 1
   if(is.null(gene.qval)) gene.qval <- simesAggregation(d$bin.pval, d$gene)
-  if(is.factor(d$gene)) d$gene <- droplevels(d$gene)
-  si <- split(seq_len(nrow(d)), d$gene)
-  d2 <- data.frame( row.names=names(si))
-  d$log10p <- -log10(d$bin.pval)
-  d2 <- vapply( si, FUN.VALUE=numeric(8), FUN=function(i){
-    w <- d$log10p[i]
-    if(sum(w,na.rm=TRUE)==0) w <- rep(1,length(i))
-    c( w.coef=weighted.mean(d$coef[i], w),
-       w.abs.coef=weighted.mean(abs(d$coef[i]), w),
-       w.width=sum(d$width[i]*w)/sum(w),
-       w.density=log(weighted.mean(d$meanLogDensity[i], w)),
-       sizeScore=weighted.mean(d$coef[i], w*d$width[i]),
-       abs.sizeScore=weighted.mean(abs(d$coef[i]), w*d$width[i]),
-       geneMeanDensity=mean(d$meanLogDensity[i], trim=0.05),
-       density.ratio=weighted.mean(d$meanLogDensity[i]-
-                                     mean(d$meanLogDensity[i]), w)
-    )
-  })
-  d2 <- as.data.frame(t(d2))
+  d$gene <- droplevels(as.factor(d$gene))
+  d$coef[is.na(d$coef)] <- 0
+  g0 <- names(which(!any(LogicalList(split(d$bin.pval<1, d$gene)))))
+  d$weight <- -log10(d$bin.pval)
+  d$weight[is.na(d$weight)] <- 0
+  d$weight[d$gene %in% g0] <- 1
+  w <- NumericList(split(d$weight, d$gene))
+  w <- w/sum(w)
+  co <- NumericList(split(d$coef, d$gene))
+  wi <- IntegerList(split(d$width, d$gene))
+  de <- NumericList(split(d$meanLogDensity, d$gene))
+  d2 <- data.frame( row.names = names(co),
+                    nb.bins = lengths(co),
+                    w.coef=sum(w*co),
+                    w.abs.coef=sum(w*abs(co)),
+                    w.width=sum(w*wi),
+                    w.density=log2(sum(w*de)),
+                    sizeScore=sum(wi/sum(wi)*w*abs(co)),
+                    abs.sizeScore=sum(wi/sum(wi)*w*co),
+                    geneMeanDensity=mean(de,trim=0.05) )
+  d2$density.ratio <- sum(w*(de-d2$geneMeanDensity))
+
   for(f in c(1:2,5:8)) d2[,f] <- round(d2[,f],3)
   d2$w.width <- as.integer(round(d2$w.width))
-  d2$nb.bins <- lengths(si)
   d2$q.value <- gene.qval[row.names(d2)]
   if("gene_name" %in% colnames(d)){
     d <- d[order(d$gene, lengths(d$gene_name)),c("gene","gene_name")]
